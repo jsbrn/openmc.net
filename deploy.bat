a840b754c8fb4ab7e82e5df40b18ed2efea07472
Jeremy <23155614+jsbrn@users.noreply.github.com>
2018-10-21 19:43:06 -0400

    Updating deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 now rm openmc.net -y
-now -y -e MONGODB_USERNAME=@mongodb_username -e MONGODB_PASSWORD=@mongodb_password
+now --public --regions sfo -y -e MONGODB_USERNAME=@mongodb_username -e MONGODB_PASSWORD=@mongodb_password
 now alias -y
+now scale openmc.net 1 1
